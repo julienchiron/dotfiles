d402006805bd282b62b038a1f8b7e399254bf3ab
Julien Chiron <82217060+julienchiron@users.noreply.github.com>
2021-04-13 13:25:33 +0200

    Updated install.sh to back existing files first

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+################################################################################
+# Install script
+# We assume that .dotfiles project is at the root of the home directory
+################################################################################
+
+if [ -f $HOME/.gitconfig ]
+then
+    mv $HOME/.gitconfig $HOME/.gitconfig-backup
+fi
+
+ln -nfs $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
+
+if [ -f $HOME/.vimrc ]
+then
+    mv $HOME/.vimrc $HOME/.vimrc-backup
+fi
+
+ln -nfs $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
